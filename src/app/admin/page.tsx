3d9e5c5ecd1250a42fb3cb279be2792ee5f1f27e
Elias Shemsu <echosierran@gmail.com>
2024-05-28 10:08:07 +0300

    Admin page cards, loading spinner

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,17 +5,77 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
 
-export default function AdminDashboard() {
+async function getSales() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+}
+
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { pricePaidInCents: true },
+    }),
+  ]);
+
+  await wait(2000);
+
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0 ? 0 : (orderData._sum.pricePaidInCents || 0) / 100,
+  };
+}
+
+async function wait(timeout: number) {
+  return new Promise((resolve) => setTimeout(resolve, timeout));
+}
+
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    db.product.count({ where: { isAvailableForPurchase: true } }),
+    db.product.count({ where: { isAvailableForPurchase: true } }),
+  ]);
+
+  return { activeCount, inactiveCount };
+}
+
+export default async function AdminDashboard() {
+  const [salesData, userData, productData] = await Promise.all([
+    getSales(),
+    getUserData(),
+    getProductData(),
+  ]);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Sales</CardTitle>
-          <CardDescription>Description</CardDescription>
-        </CardHeader>
-        <CardContent>Text</CardContent>
-      </Card>
+      <DashboardCard
+        title={"Sales"}
+        description={`${formatNumber(salesData.numberOfSales)} Orders`}
+        body={`${formatCurrency(salesData.amount)}`}
+      />
+      <DashboardCard
+        title={"Customers"}
+        description={`${formatCurrency(
+          userData.averageValuePerUser
+        )} Average Value`}
+        body={`${formatNumber(userData.userCount)}`}
+      />
+
+      <DashboardCard
+        title={"Active Products"}
+        description={`${formatNumber(productData.inactiveCount)} Inactive`}
+        body={`${formatNumber(productData.activeCount)}`}
+      />
     </div>
   );
 }
