0bf1adf38c34f865064c4503aea120c5c31783e8
Elias Shemsu <echosierran@gmail.com>
2024-05-28 10:48:52 +0300

    Product table header

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,46 @@
+import PageHeader from "./_components/pageHeader";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function AdminProductsPage() {
+  return (
+    <>
+      <div className="flex justify-between">
+        <PageHeader>Products</PageHeader>;
+        <Button asChild>
+          <Link href={"/admin/products/new"}>Add Product</Link>
+        </Button>
+      </div>
+      <ProductsTable />
+    </>
+  );
+}
+
+function ProductsTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available for Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+    </Table>
+  );
+}
