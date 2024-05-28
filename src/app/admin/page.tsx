de37a35ddf235ec2069882d12ae692157fb6688b
Elias Shemsu <echosierran@gmail.com>
2024-05-28 09:17:35 +0300

    Prisma singleton setup

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,39 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function AdminDashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Sales</CardTitle>
+          <CardDescription>Description</CardDescription>
+        </CardHeader>
+        <CardContent>Text</CardContent>
+      </Card>
+    </div>
+  );
+}
+
+type DashboardCardProps = {
+  title: String;
+  description: String;
+  body: String;
+};
+
+function DashboardCard({ title, description, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>{body}</CardContent>
+    </Card>
+  );
 }
